cd69afeabbd0fe73befcf8a4acb275ec37913413
Adam Capulong <adam.capulong@gmail.com>
2014-12-01 21:20:16 -0500

    taking into consideration previous entry's improvements

--- ps1b.py
+++ ps1b.py
@@ -1,33 +1,28 @@
 #compute ratios of the sum of the logs of primes to the log of the sum of primes
 
-from math import sqrt
-from math import log
+listOfPrimes = []
+counter = 2
+sumlog = 0
+logsum = 0
 
-def isPrime(x):
+def testIfPrime(x):
 	isprime = True
 	if x == 2:
-		return isprime
+		return True
 	elif x%2 == 0:
-		isprime = False
-		return isprime
+		return False
 	else: 
 		for y in listOfPrimes:
 			if y <= (int(sqrt(x))+1):
 				if x%y == 0:
 					isprime = False
-					
-	return isprime
-
-listOfPrimes = [2]
-counter = 3
+                return isprime
 
 while len(listOfPrimes) < 1000:
-	if isPrime(counter):
+	if testIfPrime(counter):
 		listOfPrimes.append(counter)
-	counter = counter + 2	
+	counter = counter + 1	
 
-sumlog = 0
-logsum = 0
 	
 for j in listOfPrimes:
 	sumlog = sumlog + log(j)
