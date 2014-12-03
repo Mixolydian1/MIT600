3dae306c5639fd42edbdde5c263059b4aba21077
Adam Capulong <adam.capulong@gmail.com>
2014-12-02 21:02:52 -0500

    Structured to answer the problem set question.

--- ps2c.py
+++ ps2c.py
@@ -1,7 +1,30 @@
 # McDiophantine
 
-target = 70
+start = 55
 
+def solveCombination(target):
+	six = 0
+	nine = 0
+	twenty = 0
+	solved = False
+	for maxTwenty in reversed(range(0, target/20+1)):
+		for maxNine in reversed(range(0,(target-20*maxTwenty)/9+1)):
+			for maxSix in reversed(range(0, (target - 20*maxTwenty - 9*maxNine)/6+1)):
+				# print "Guess: " + str(maxSix) + "(6) + " + str(maxNine) + "(9) + " + str(maxTwenty) + "(20)"
+				if 6*maxSix + 9*maxNine + 20*maxTwenty == target:
+					solved = True
+					six = maxSix
+					nine = maxNine
+					twenty = maxTwenty
+
+	return solved
+
+for x in reversed(range(20,start)):
+	if solveCombination(x) == False:	
+		print "number: " + str(x) + " is unobtainable."
+		break
+
+'''
 def solveCombination(target):
 	six = 0
 	nine = 0
@@ -23,7 +46,11 @@ def solveCombination(target):
 	if solved == True:
 		return "Solution: " + str(maxSix) + "(6) + " + str(maxNine) + "(9) + " + str(maxTwenty) + "(20) = " + str(target)
 
-print solveCombination(target)
+'''
+
+
+
+
 
 
 
