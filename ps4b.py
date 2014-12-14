f75af561afb19708defb1fdbfe8d910eb1992616
Mixolydian1 <adam.capulong@gmail.com>
2014-12-14 17:04:12 -0500

    easy addition to model

---
+++ ps4b.py
@@ -0,0 +1,30 @@
+# Adam Capulong
+
+def nestEggVariable(salary, save, growthRates):
+    """
+    - salary: the amount of money you make each year.
+    - save: the percent of your salary to save in the investment account each
+      year (an integer between 0 and 100).
+    - growthRate: a list of the annual percent increases in your investment
+      account (integers between 0 and 100).
+    - return: a list of your retirement account value at the end of each year.
+    """
+
+    fund = 0
+    record = []
+    for x in range(0,len(growthRates)):
+    		fund = fund*(1+0.01*growthRates[x])
+    		fund = fund + salary*save*0.01
+    		record.append(fund)
+    return record
+
+def testNestEggVariable():
+    salary      = 10000
+    save        = 10
+    growthRates = [3, 4, 5, 0, 3]
+    savingsRecord = nestEggVariable(salary, save, growthRates)
+    return savingsRecord
+    # Output should have values close to:
+    # [1000.0, 2040.0, 3142.0, 4142.0, 5266.2600000000002]
+
+print testNestEggVariable()
