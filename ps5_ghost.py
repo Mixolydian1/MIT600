7d9a56723542528d4ef7563eb9a8f7d268d0f994
Mixolydian1 <adam.capulong@gmail.com>
2014-12-14 22:19:36 -0500

    simple ghost game, works please check

--- ps5_ghost.py
+++ ps5_ghost.py
@@ -1,14 +1,6 @@
 # Problem Set 5: Ghost
-# Name: 
-# Collaborators: 
-# Time: 
-#
+# Name: Adam Capulong
 
-import random
-
-# -----------------------------------
-# Helper code
-# (you don't need to understand this helper code)
 import string
 
 WORDLIST_FILENAME = "words.txt"
@@ -46,12 +38,58 @@ def get_frequency_dict(sequence):
     return freq
 
 
-# (end of helper code)
-# -----------------------------------
 
 # Actually load the dictionary of words and point to it with 
 # the wordlist variable so that it can be accessed from anywhere
 # in the program.
 wordlist = load_words()
 
-# TO DO: your code begins here!
+
+print "Welcome to Ghost by Adam Capulong"
+
+# boolean to track when game ends
+gameOver = False
+# player 1 = 1
+activePlayer = 1
+# track the current word fragment
+currentWord = ""
+
+while not gameOver:
+    # remind the users of whose turn it is
+    if activePlayer == 1:
+        cmd = raw_input("Player 1 turn: ")
+        print "Player 1 says letter: " + cmd.upper()
+    else:
+        cmd = raw_input("Player 2 turn: ")
+        print "Player 2 says letter: " + cmd.upper()
+
+
+    # add the lower-case letter to the end of the currentWord
+    currentWord = currentWord + cmd.lower()
+    print "Current word fragment: " + currentWord.upper()
+
+    fragmentFound = False
+    for word in wordlist:
+        if currentWord == word[:len(currentWord)]:
+            fragmentFound = True
+        if len(currentWord) > 3:
+            if word == currentWord:
+                gameOver = True
+                if activePlayer == 1:
+                    print "Player 1 formed word", currentWord
+                    print "Player 2 wins!"
+                else:
+                    print "Player 2 formed word", currentWord
+                    print "Player 1 wins!"
+
+    if fragmentFound == False:
+        gameOver = True
+        print currentWord, "does not begin a word."
+        if activePlayer == 1:
+            print "Player 2 wins!"
+        else:
+            print "Player 1 wins!"
+
+    activePlayer = activePlayer*-1
+
+
